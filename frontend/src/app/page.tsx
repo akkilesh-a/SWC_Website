643717b576b4aa31f9fb8c713581a706f5af7686
Akkilesh-A <akkilalagar05@gmail.com>
2025-01-04 18:00:48 +0530

    refactor: move example components to /example

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,8 @@
-import { CustomComponent, SanityExample } from '@/components'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div className='p-8 space-y-10'>
-      <h1 className='text-2xl font-bold'>Standard Coding Practices</h1>
-      <CustomComponent />
-      <SanityExample />
+    <div>
     </div>
   )
 }
