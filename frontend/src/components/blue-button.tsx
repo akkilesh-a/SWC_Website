2731ecebb8f3077bd42cbcab97d081cf2133b720
VeerViswajit <veerviswajit2004@gmail.com>
2025-01-07 23:02:18 +0530

    refactor: change blue-button style

--- frontend/src/components/blue-button.tsx
+++ frontend/src/components/blue-button.tsx
@@ -9,12 +9,7 @@ const BlueButton: React.FC<BlueButtonProps> = ({ text }) => {
   return (
     <div>
       <Button
-        className="bg-[#00194E] hover:bg-[#00194E] text-white font-dmSerifText text-[25px] font-normal leading-[34.28px] text-center w-[364px] h-[81px] rounded-tl-[11px]"
-        style={{
-          textUnderlinePosition: 'from-font',
-          textDecorationSkipInk: 'none',
-          
-        }}
+        className="bg-[#00194E] hover:bg-[#00194E] text-white font-dmSerifText text-[25px] font-normal leading-[34.28px] text-center w-[364px] h-[81px] rounded-tl-[11px] underline-offset-auto decoration-auto"
       >
         {text}
       </Button>
