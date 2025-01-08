d466c5e9fd897581bdfa71973e35d12dc248747e
Akkilesh-A <akkilalagar05@gmail.com>
2025-01-08 12:00:00 +0530

    feat: add latest news letter

--- frontend/src/app/newsletter/page.tsx
+++ frontend/src/app/newsletter/page.tsx
@@ -1,11 +1,46 @@
 import { LandingImageWithContent } from '@/components'
-import React from 'react'
+import { client } from '@/sanity/client';
+import { defineQuery } from 'next-sanity';
 
-const NewsLettersPage = () => {
+const FETCH_NEWSLETTERS = defineQuery(`*[
+    _type=="newsletter"  
+    ]{
+      _id,
+      link,
+      date
+    }`)
+
+async function NewsLettersPage(){
+  const data=await client.fetch(FETCH_NEWSLETTERS)
+  // console.log(data)
+  // const data=[
+  //   {
+  //     _id: '9a528bb5-6f58-4ba6-90d9-e0aee07c8369',
+  //     link: 'https://online.pubhtml5.com/stgnz/oegu/',
+  //     date: '2024-11-01'
+  //   },
+  //   {
+  //     _id: '9d6d5dcd-2a28-484c-8be2-56bba4b0b0d2',
+  //     link: 'https://online.pubhtml5.com/stgnz/natk',
+  //     date: '2024-10-01'
+  //   }
+  // ]
+
   return (
     <div>
-        <LandingImageWithContent variant='text' heading='News Letters' subheading='Student Welfare Committee' />
+        <div>
+          <LandingImageWithContent variant='text' heading='News Letters' subheading='Student Welfare Committee' />
+        </div>
+        <div className='bg-white flex space-y-8 flex-col items-center justify-center p-16'>
+          <div className='text-3xl md:text-6xl font-bold underline font-dmSerifDisplay'>Latest Newsletter</div>
+          <iframe
+            className="bg-white border border-white w-[400px] sm:w-[600px] md:w-[1000px] h-[500px]"
+            key={data[0]._id}
+            src={data[0].link}
+          />
+        </div>
     </div>
+
   )
 }
 
